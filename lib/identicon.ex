c811a5d37b8843fb90e595f3975b97bee93702a8
Nellie K <noircir@users.noreply.github.com>
2019-01-10 20:59:24 -0500

    add helper function mirror_row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,7 +12,46 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
   end
+
+  def hash_input(input) do
+    seed = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    # seed is an array of 16 hex numbers;
+    # we'll feed it into the Image struct
+    %Identicon.Image{seed: seed}
+  end
+
+  @doc """
+  Picks first three numbers out of the array, which will represent R, G, B
+
+  """
+  def pick_color(%Identicon.Image{seed: [r, g, b | _tail]} = image) do
+    # Not modifing any image; creating a new one by copying 
+    # the whole image (which is {seed: seed}), and specifying the "color"
+    # property of the struct.
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{seed: seed} = image ) do
+    seed
+    |> Enum.chunk_every(3)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    # [145, 46, 200]
+    # [first, second | _tail] = row
+
+    # [145, 46, 200, 46, 145]
+    # row ++ [second, first]
+
+    # [first, second | _tail] = row
+    [first, second] ++ Enum.reverse(row)
+  end
+
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,7 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Identicon.hello() == :world
+  # end
 end
